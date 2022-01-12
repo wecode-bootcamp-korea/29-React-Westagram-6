a0cf47445ef9fde26e89c71e1035c523703261a8
임채현 <imchaehyeon@imchaehyeon-ui-MacBookAir.local>
2022-01-12 16:31:11 +0900

    Fix: 불필요한 태그 및 주석 제거

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import LoginSoojin from "./pages/soojin/Login/Login";
-import MainSoojin from "./pages/soojin/Main/Main";
+import LoginSoojin from './pages/soojin/Login/Login';
+import MainSoojin from './pages/soojin/Main/Main';
 
-import LoginChaehyun from "./pages/chaehyun/Login/Login";
-import MainChaehyun from "./pages/chaehyun/Main/Main";
+import LoginChaehyun from './pages/chaehyun/Login/Login';
+import MainChaehyun from './pages/chaehyun/Main/Main';
 
-import LoginChungkyu from "./pages/chungkyu/Login/Login";
-import MainChungkyu from "./pages/chungkyu/Main/Main";
+import LoginChungkyu from './pages/chungkyu/Login/Login';
+import MainChungkyu from './pages/chungkyu/Main/Main';
 
-import LoginMooseon from "./pages/mooseon/Login/Login";
-import MainMooseon from "./pages/mooseon/Main/Main";
+import LoginMooseon from './pages/mooseon/Login/Login';
+import MainMooseon from './pages/mooseon/Main/Main';
 
 <Route path="/login-chaehyun" element={<LoginChaehyun />} />;
 <Route path="/login-soojin" element={<LoginSoojin />} />;
