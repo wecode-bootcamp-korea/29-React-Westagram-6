dbf3baa72380d5a90c486d60b7add31f6c4804f1
soojjung <89066676+soojjung@users.noreply.github.com>
2022-01-13 17:33:51 +0900

    Nav upload

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="wrap">
+      <div className="item">Westagram</div>
+      <div className="item">
+        <input type="text" placeholder="Search" />
+      </div>
+      <ul className="item" id="icons">
+        <li>
+          <img alt="Home icon" src="/images/soojin/iconHome.jpg" />
+        </li>
+        <li>
+          <img alt="Dm icon" src="/images/soojin/iconDm.jpg" />
+        </li>
+        <li>
+          <img alt="Upload icon" src="/images/soojin/iconUpload.jpg" />
+        </li>
+        <li>
+          <img alt="Explore icon" src="/images/soojin/iconExplore.jpg" />
+        </li>
+        <li>
+          <img alt="Heart icon" src="/images/soojin/iconHeart.jpg" />
+        </li>
+        <li>
+          <img alt="Profile icon" src="/images/soojin/iconProfile.jpg" />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
