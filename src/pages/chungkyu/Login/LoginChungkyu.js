a896dac2a1f31dfd5544b028f3eda01b203147eb
dovra_ <chungkyukim7@gmail.com>
2022-01-17 17:24:09 +0900

    Add:Mission 1 - 사용자 입력 데이터 저장 기능구현, Login,Main페이지 옮기기

--- src/pages/chungkyu/Login/LoginChungkyu.js
+++ src/pages/chungkyu/Login/LoginChungkyu.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import './Login.scss';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function LoginChungkyu() {
-  return <div className="loginChungkyu">청규 로그인 페이지</div>;
+  const [active, setActive] = useState();
+
+  console.log('start', active);
+  const handleIdinPut = event => {
+    setActive(event.target.value);
+  };
+  const handleIdinPw = event => {
+    setActive(event.target.value);
+  };
+  return (
+    <div>
+      <link href="style/login.css" rel="stylesheet" type="text/css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+        rel="stylesheet"
+      />
+
+      <div className="viewp">
+        <div className="centerwrapper">
+          <nav className="title">Westagram</nav>
+          <div className="input-wrap">
+            <input
+              id="inputid"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={handleIdinPut}
+            />
+            <input
+              id="inputpw"
+              type="text"
+              placeholder="비밀번호"
+              onChange={handleIdinPw}
+            />
+          </div>
+          <div className="button-wrap">
+            <Link to="/Main-chungkyu">
+              {' '}
+              <button id="login"> 로그인</button>
+            </Link>
+          </div>
+          <div className="forgetpw">
+            <a href>비밀번호를 잊으셨나요?</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginChungkyu;
--- src/pages/chungkyu/Main/MainChungkyu.js
+++ src/pages/chungkyu/Main/MainChungkyu.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import './MainChungkyu.scss';
+import Nav from '../../../components/Nav/Nav';
 
 function MainChungkyu() {
-  return <div className="mainChungkyu">청규 메인 페이지</div>;
+  return (
+    <div className="mainChungkyu">
+      <link href="./style/main.css" rel="stylesheet" type="text/css" />
+      <div>
+        <main>
+          <Nav />
+          <div className="feedwrapper">
+            <div className="feeds">
+              <article>
+                <img
+                  src="images/chungkyu/다운로드.jpeg"
+                  width="100%"
+                  height="100%"
+                />
+              </article>
+              <span className="contact">
+                <i className="far fa-heart fa-2x" />
+                <i className="far fa-comment fa-2x" />
+                <i className="far fa-paper-plane fa-2x" />
+              </span>
+              <ul className="list">
+                <li>
+                  <span className="name">chungkyukim</span>
+                  <span>런각인데.....</span>
+                </li>
+              </ul>
+              <div className="comment">
+                <input
+                  calss="commentinput"
+                  type="text"
+                  placeholder="댓글을 입력하세요.."
+                />
+                <button className="button">게시</button>
+              </div>
+            </div>
+            <div className="main-rights" />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default MainChungkyu;
