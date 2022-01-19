8e766431952e4557e742814cce29c54019d64799
soojjung <89066676+soojjung@users.noreply.github.com>
2022-01-19 13:52:18 +0900

    Add: Link 기능 추가

--- src/pages/soojin/Login/LoginSoojin.js
+++ src/pages/soojin/Login/LoginSoojin.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './LoginSoojin.scss';
 
 function LoginSoojin() {
@@ -53,9 +54,13 @@ function LoginSoojin() {
               placeholder="비밀번호"
             />
           </div>
-          <button disabled={isValidate} className="westaButton">
-            로그인
-          </button>
+          <div className="buttonWrapper">
+            <Link to="/main-soojin">
+              <button disabled={isValidate} className="westaButton">
+                로그인
+              </button>
+            </Link>
+          </div>
         </div>
         <a
           href="https://www.instagram.com/sem/campaign/emailsignup/?campaign_id=13530338586&extra_1=s|c|547419126431|e|instagram%20%27|&placement=&creative=547419126431&keyword=instagram%20%27&partner_id=googlesem&extra_2=campaignid%3D13530338586%26adgroupid%3D126262418054%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-1321618851291%26loc_physical_ms%3D1009871%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiA2sqOBhCGARIsAPuPK0jcdlJphJovnl18oPxBsiA-k2HHGhrA-DCugKCsTl_QYfTL4GUxcakaAi7REALw_wcB"
